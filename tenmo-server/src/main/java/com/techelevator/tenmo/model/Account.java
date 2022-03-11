48aeb3c3133b51e50f3db762f2e1ced1e93849f8
ziye liu <lzy89412@hotmail.com>
2022-03-11 17:08:25 -0500

    add account class on server side

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -0,0 +1,52 @@
+package com.techelevator.tenmo.model;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+
+public class Account {
+    private long accountId;
+    @NotBlank(message = "UserId cannot be blank.")
+    private long userId;
+    @DecimalMin("0")
+    private BigDecimal balance = new BigDecimal(1000);
+
+    public Account(long accountId, long userId, BigDecimal balance) {
+        this.accountId = accountId;
+        this.userId = userId;
+        this.balance = balance;
+    }
+
+    public long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(long accountId) {
+        this.accountId = accountId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountId=" + accountId +
+                ", userId=" + userId +
+                ", balance=" + balance +
+                '}';
+    }
+}
