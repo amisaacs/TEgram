3f4c0676c4c4673183bf217742e2d5724571d313
ziye liu <lzy89412@hotmail.com>
2022-03-12 16:52:42 -0500

    change back to original code from last night

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -6,6 +6,5 @@ import java.math.BigDecimal;
 
 public interface TransferDao {
     void makeTransfer (Transfer transfer, BigDecimal senderBalance);
-    Transfer createTransfer(Transfer newTransfer);
-    boolean updateAccount(Transfer transfer, BigDecimal senderBalance);
+
 }
